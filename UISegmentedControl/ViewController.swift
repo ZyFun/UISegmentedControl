baf3babc85e8d42c2afe577e1c3104d694a87863
Dmitriy Danilin <danilindn@icloud.com>
2020-07-08 13:42:27 +0300

    Окончание 2 урока

--- UISegmentedControl/ViewController.swift
+++ UISegmentedControl/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segmenteControl: UISegmentedControl!
+    @IBOutlet weak var textLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Конфигурируем textLabel
+        textLabel.isHidden = true
+        textLabel.font = textLabel.font.withSize(35)
+        textLabel.textAlignment = .center
+        textLabel.numberOfLines = 2
+        
+        //Добавляем новый сегмент
+        segmenteControl.insertSegment(withTitle: "Third", at: 2, animated: true)
+        
     }
 
-
+    @IBAction func choiseSegmented(_ sender: UISegmentedControl) {
+        
+        textLabel.isHidden = false
+        
+        switch segmenteControl.selectedSegmentIndex {
+        case 0:
+            textLabel.text = "The first segment is selected"
+            textLabel.textColor = .red
+        case 1:
+            textLabel.text = "The second segment is selected"
+            textLabel.textColor = .blue
+        case 2:
+            textLabel.text = "The third segment is selected"
+            textLabel.textColor = .purple
+        default:
+            print("Oops")
+        }
+    }
+    
 }
 
